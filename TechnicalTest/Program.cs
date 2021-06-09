cfe7dc081d8389ed16af7623bb1c36ceaf8e8869
anthonymagann <anthony.magann2@mail.dcu.ie>
2021-06-09 12:50:41 +0100

    Workspace

---
+++ TechnicalTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechnicalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
