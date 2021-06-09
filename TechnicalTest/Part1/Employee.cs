ad6da72d1f9b23c75a321142eb30b43c35767ec2
anthonymagann <anthony.magann2@mail.dcu.ie>
2021-06-09 13:02:41 +0100

    Part1

---
+++ TechnicalTest/Part1/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechnicalTest.Part1
+{
+    class Employee
+    {
+    }
+}
---
+++ TechnicalTest/Part1/EmployeeRepo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TechnicalTest.Part1
+{
+    public class EmployeeRepo
+    {
+        private List<Employee> Employees { get; }
+
+        public EmployeeRepo()
+        {
+            Employees = new List<Employee>();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+    }
+}
